a342ff9ba6293e1310a03a8ea746ffac757cadeb
anguslmm <anguslmm@gmail.com>
2016-06-02 19:55:49 +0000

    Add fundraising app and .gitignore

---
+++ pullupsforpatriots/fundraising/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Marine model goes here (replace this line with it).
+
+# Donor model goes here (replace this line with it).
+
+# Sponsor model goes here (replace this line with it).
+
+# Donation model goes here (replace this line with it).
