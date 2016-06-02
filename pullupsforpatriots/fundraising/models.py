8e0c93de39701a60035800d1ac89d8147e0fed78
Radovan <radsquest@gmail.com>
2016-06-02 20:24:47 +0000

    implement the donor class

--- pullupsforpatriots/fundraising/models.py
+++ pullupsforpatriots/fundraising/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Marine model goes here (replace this line with it).
 
 # Donor model goes here (replace this line with it).
+class Donor(models.Model):
+    name=models.CharField(max_length=200)
+   
 
 # Sponsor model goes here (replace this line with it).
 
