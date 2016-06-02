5855eaa51cfd9aa3a6817eb2eefa1cd566e4036c
Radovan <radsquest@gmail.com>
2016-06-02 20:32:41 +0000

    delete whitespace

--- pullupsforpatriots/fundraising/models.py
+++ pullupsforpatriots/fundraising/models.py
@@ -2,11 +2,10 @@ from django.db import models
 
 # Marine model goes here (replace this line with it).
 
-# Donor model goes here (replace this line with it).
+
 class Donor(models.Model):
     name=models.CharField(max_length=200)
    
-
 # Sponsor model goes here (replace this line with it).
 
 # Donation model goes here (replace this line with it).
