629e8cdbdacbb662926bd6919eedef3a1f9546e7
Christian Ruiz <caruiz1996@gmail.com>
2016-06-02 20:38:17 +0000

    Input max_digit and decimal_places for amount raised.

--- pullupsforpatriots/fundraising/models.py
+++ pullupsforpatriots/fundraising/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Marines(models.Model):
     name = models.CharField(max_length=200)
-    amount_raised = models.DecimalField('Amount Raised')
+    amount_raised = models.DecimalField('Amount Raised', max_digits=9, decimal_places=2)
 
 # Donor model goes here (replace this line with it).
 
