cf1092e8614843a4d68e0674018169519d994e75
anguslmm <anguslmm@gmail.com>
2016-06-08 00:20:37 +0000

    Fix various typos and bugs.

--- pullupsforpatriots/fundraising/urls.py
+++ pullupsforpatriots/fundraising/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import  url
 from . import views
 
 urlpatterns = [
-    url(r'^donations/ourgoal/$', 'views.placeholder', name='ourgoal'),
-    url(r'^donations/marines/$', 'views.placeholder', name='marines'),
-    url(r'^donations/marines/search/$', 'views.placeholder', name='marines'),
-    url(r'^donations/marines/all/$', 'views.placeholder', name='marines'),
-    url(r'^donations/marines/(?P<pk>[0-9]+)/$', 'views.placeholder', name='marines'),
-    url(r'^donations/sponsors/$', 'views.placeholder', name='sponsors'),
-    url(r'^donations/donors/$', 'views.placeholder', name='donors'),
+    url(r'^$', views.placeholder, name='index'),
+    url(r'^ourgoal/$', views.placeholder, name='ourgoal'),
+    url(r'^marines/$', views.placeholder, name='marines'),
+    url(r'^marines/search/$', views.placeholder, name='marines'),
+    url(r'^marines/all/$', views.placeholder, name='marines'),
+    url(r'^marines/(?P<pk>[0-9]+)/$', views.placeholder, name='marines'),
+    url(r'^sponsors/$', views.placeholder, name='sponsors'),
+    url(r'^donors/$', views.placeholder, name='donors'),
     ]
--- pullupsforpatriots/fundraising/views.py
+++ pullupsforpatriots/fundraising/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def placeholder(request):
+def placeholder(request, pk=0):
     return HttpResponse("Hello, world. This is a placeholder view.")
