9592573c1533cc46f579defdff4874aad2ba6d7b
anguslmm <anguslmm@gmail.com>
2016-06-09 16:47:15 +0000

    Prepare for views and templates.

--- pullupsforpatriots/pullupsforpatriots/settings.py
+++ pullupsforpatriots/pullupsforpatriots/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'pullupsforpatriots.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(PROJECT_ROOT, 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
