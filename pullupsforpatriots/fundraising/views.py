332631118f5fd56a964b8854986bd10fb28de668
Drake <drayc99@gmail.com>
2016-06-11 23:52:01 +0000

    Views

--- pullupsforpatriots/fundraising/views.py
+++ pullupsforpatriots/fundraising/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
 def placeholder(request, pk=0):
-    return HttpResponse("Hello, world. This is a placeholder view.")
+    return HttpResponse("Hello, world. This is a placeholder view."
+    
+class OurGoalView(generic.ListView):
+    template_name = 'template/ourgoal.html'
+    
+class MarinesView(generic.DetailedView):
+    template_name = 'template/marines.html'
+    
+class SponsorView(generic.ListView):
+    template_name = 'template/sponsor.html'
+
+class DonorsView(generic.ListView):
+    template_name = 'template/donors.html'
+    
