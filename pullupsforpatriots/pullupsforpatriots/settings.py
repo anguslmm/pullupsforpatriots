cff3dc5ac0c0572a855e53ceead142adac46f6da
anguslmm <anguslmm@gmail.com>
2016-06-12 19:39:16 +0000

    Implement Marine detail page and fix small errors

--- pullupsforpatriots/pullupsforpatriots/settings.py
+++ pullupsforpatriots/pullupsforpatriots/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'fundraising.apps.FundraisingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
