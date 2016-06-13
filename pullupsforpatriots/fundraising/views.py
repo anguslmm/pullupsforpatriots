2f6f158468e18cc5c4577b744abf9353e6386158
Drake <drayc99@gmail.com>
2016-06-13 18:22:18 +0000

    Views

--- pullupsforpatriots/fundraising/views.py
+++ pullupsforpatriots/fundraising/views.py
@@ -2,22 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 
-from .models import Marines, Sponsor
+from .models import Marines, Sponsor, Donation
 
-class IndexView(generic.ListView):
+class IndexView(generic.DetailView):
     template_name = 'template/index.html'
     
-class OurGoalView(generic.ListView):
+class OurGoalView(generic.DetailView):
     template_name = 'template/ourgoal.html'
     
-class MarinesView(generic.ListView):
+class MarinesView(generic.DetailView):
     model = Marines
     template_name = 'template/marines.html'
     
-class SponsorsView(generic.ListView):
+class SponsorsView(generic.DetailView):
     model = Sponsor
     template_name = 'template/sponsor.html'
 
-class DonorsView(generic.ListView):
+class DonorsView(generic.DetailView):
+    model = Donation
     template_name = 'template/donors.html'
     
